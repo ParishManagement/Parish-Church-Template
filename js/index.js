2c5ba4dac5afff2b9760fe9a18928b7e5b75d93b
NaveenTRL <naveen.y@tringapps.com>
2015-06-11 20:48:38 +0530

    June11th Gallery Overlay Changes

--- js/index.js
+++ js/index.js
@@ -59,9 +59,10 @@ $(function(){
 						+'<div class="row">'
 							+'<div class="profilePicContainer col-sm-3 col-md-3 col-lg-3" style="height:150px;">'
 								+'<img src="images/thobith.jpg" style="max-height:100%;" alt="profilePic" class="img-responsive" />'
-								/* +'<div style="position:absolute;">'
+								+'<div style="position:absolute;" class="myCustomLableForChangeImage">'
 									+'<input type="file" name="profilePic" />'
-								+'</div>' */
+									+'<span>Change Image</span>'
+								+'</div>'
 							+'</div>'
 							+'<div class="profileDetails col-sm-9 col-md-9 col-lg-9">'
 								+'<div class="row">'
@@ -113,18 +114,13 @@ $(function(){
 		var currentTarget = $(e.currentTarget);
 		e.preventDefault();
 		window.scrollTo(0,1);
-		var html = '<div>'
+		var html = '<div style="height:100%;">'
 						+'<h3>Gallery</h3>'
-						+'<div>'
-							+'<div class="profilePicContainer">'
-								+'<img src="images/naveen.jpg" alt="profilePic" class="img-responsive" />'
-							+'</div>'
-							+'<div class="profileDetails">'
-							+'</div>'
-						+'</div>'
-						+'<div>'
-							+'<a href="#gallery">Gallery</a>'
-							+'<a href="#recentNews">Recent News</a>'
+						+'<div class="row">'
+							+'<div class="col-md-3 col-sm-3 col-lg-3"><img class="img-responsive" src="http://2.bp.blogspot.com/-H6MAoWN-UIE/TuRwLbHRSWI/AAAAAAAABBk/89iiEulVsyg/s400/Free%2BNature%2BPhoto.jpg" /></div>'
+							+'<div class="col-md-3 col-sm-3 col-lg-3"><img class="img-responsive" src="http://www.virginia.org/uploadedImages/virginiaorg/Images/OrgImages/H/HamptonConventionVisitorBureau/Grandview_Nature_Preserve.jpg?width=300&height=200&scale=upscalecanvas" /></div>'
+							+'<div class="col-md-3 col-sm-3 col-lg-3"><img class="img-responsive" src="http://blog.arborday.org/wp-content/uploads/2013/02/NEC1-300x200.jpg" /></div>'
+							+'<div class="col-md-3 col-sm-3 col-lg-3"><img class="img-responsive" src="http://th03.deviantart.net/fs70/200H/f/2010/256/0/9/painting_of_nature_by_dhikagraph-d2ynalq.jpg" /></div>'
 						+'</div>'
 				   +'</div>';
 		displayOverlay(html);
