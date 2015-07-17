480e0caa5d74176a3fddd7992dc9a8b98558a66b
NaveenTRL <naveen.y@tringapps.com>
2015-07-17 18:47:06 +0530

    Church page changes

--- Version-2/js/index.js
+++ Version-2/js/index.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
+	//$("body").lionbars();
 	$('html,body').animate({scrollTop: 0}, 1000);
 	$(window).scroll(function(e){
 		return false;
+	});
+	$('#memCalender').fullCalendar({
+		// put your options and callbacks here
+		weekends: false,
+		dayClick: function() {
+			alert('a day has been clicked!');
+		}
 	});
 });
