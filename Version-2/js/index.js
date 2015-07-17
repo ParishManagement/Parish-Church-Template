989e206379c0beb901d4e0ed15ff618f2c3c6513
NaveenTRL <naveen.y@tringapps.com>
2015-07-18 00:49:05 +0530

    MEM Calender changes

--- Version-2/js/index.js
+++ Version-2/js/index.js
@@ -6,9 +6,57 @@ $(document).ready(function(){
 	});
 	$('#memCalender').fullCalendar({
 		// put your options and callbacks here
-		weekends: false,
-		dayClick: function() {
-			alert('a day has been clicked!');
-		}
+		header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultView: 'month',
+			editable: true
+			/* defaultDate: '2014-06-12',
+			defaultView: 'month',
+			editable: true, */
+			/* events: [
+				{
+					title: 'All Day Event',
+					start: '2014-06-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2014-06-07',
+					end: '2014-06-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-06-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-06-16T16:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2014-06-12T10:30:00',
+					end: '2014-06-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2014-06-12T12:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2014-06-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2014-06-28'
+				}
+			] */
 	});
+	
+	//$(".fc-day-grid-container").css("height","337px");
+	
 });
